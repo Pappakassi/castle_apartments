bd290ba780c5da312f7163c4d42565169c8c0c18
haukurv24 <haukurv24@ru.is>
2025-05-13 12:50:02 +0000

    local images

--- apartments/views.py
+++ apartments/views.py
@@ -56,6 +56,7 @@ def create_apartment(request):
 
 def apartments_list(request):
     apartments = Apartment.objects.all()
+    search = request.GET.get('search_filter')
 
     # Search by street name (case-insensitive)
     search = request.GET.get('search_filter')
