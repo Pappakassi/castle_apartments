35c582621c5442d6697e616eed660de14880feca
Pappakassi <144326577+Pappakassi@users.noreply.github.com>
2025-05-14 15:22:50 +0000

    added some data etc.

--- apartments/urls.py
+++ apartments/urls.py
@@ -4,12 +4,14 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', views.apartments_list, name='apartments_list'),  # ✅ this handles /apartments/
+    path('', views.apartments_list, name='apartments_list'),
     path('<int:pk>/', views.apartment_detail, name='apartment_detail'),
     path('create_apartment/', views.create_apartment, name='create_apartment'),
     path('<int:pk>/delete/', views.delete_apartment, name='delete_apartment'), #Arnar er með þetta öfugt, þ.e.a.s. /delete/<int:id>/
     path('<int:pk>/edit/', views.update_apartment, name='update_apartment'),
     path('sellers/<int:pk>/', views.seller_detail, name='seller_detail'),
+    path('sellers/list/', views.seller_list, name='seller_list'),
+
     path('offers/', include('offers.urls')),
 
 
--- apartments/views.py
+++ apartments/views.py
@@ -10,7 +10,11 @@ from django.db.models import Q
 from apartments.forms.apartment_create_form import ApartmentCreateForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+from .models import Seller
 
+def seller_list(request):
+    sellers = Seller.objects.all()
+    return render(request, 'apartments/seller_list.html', {'sellers': sellers})
 
 
 
